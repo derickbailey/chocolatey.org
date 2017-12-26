38dc6f1af3f036bc68e26ebf03f150da0aaf9793
Derick Bailey <derickbailey@gmail.com>
2017-12-26 10:32:24 -0600

    script to hide the cookies notice

--- chocolatey/Website/Scripts/CookiesNotice/Index.js
+++ chocolatey/Website/Scripts/CookiesNotice/Index.js
@@ -1,9 +1,9 @@
-﻿var Closeable = (function () {
+﻿var CookiesNotice = (function () {
 
     return {
-        modal: function(cookieName) {
-            $(".modal-closeable .close").click(function (e) {
-                $(e.target).closest(".modal-closeable").hide();
+        modal: function (cookieName) {
+            $(".cookies-popup .cookies-close").click(function (e) {
+                $(e.target).closest(".cookies-popup").hide();
                 var d = new Date();
                 // 100 years in milliseconds: 100 years * 365 days * 24 hours * 60 minutes * 60 seconds * 1000ms
                 d.setTime(d.getTime() + (100 * 365 * 24 * 60 * 60 * 1000));
